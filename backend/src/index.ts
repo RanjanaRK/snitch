664c7bd7873d4fb2339a056c9a10e5a516772ee7
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-28 17:26:37 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
 import app from "./server.js";
 
 connectDB();
+
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
