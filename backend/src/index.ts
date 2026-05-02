fc3caafb34490d8e3946465a4dd49b4f6eebe377
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-02 21:02:59 +0530

    google auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,39 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import connectDB from "./config/db.js";
+import env from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import app from "./server.js";
-import express from "express";
-import cors from "cors";
 
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
+);
+
+app.use(passport.initialize());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/api/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    },
+  ),
 );
 
 app.use("/api/auth", authRouter);
