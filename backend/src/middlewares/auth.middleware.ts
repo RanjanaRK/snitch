5b13e3e1bde3b21beac43625646de7b511178eee
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-04 22:36:35 +0530

    Update auth.middleware.ts

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,20 +1,31 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export const authenticateSeller = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const token = req.cookies.token;
+export const authenticateRole = (allowedRoles: string[]) => {
+  const middlware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.token;
 
-  if (!token) {
-    res.status(401).json({ message: "Unauthorized" });
-  }
+    if (!token) {
+      res.status(401).json({ message: "Unauthorized" });
+    }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
-      userId: string;
-    };
-  } catch (error) {}
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+        userId: string;
+        role: string;
+      };
+
+      if (!allowedRoles.includes(decoded.role)) {
+        res.status(403).json({ message: "Forbidden" });
+      }
+
+      req.user = decoded;
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+  };
+
+  return middlware;
 };
