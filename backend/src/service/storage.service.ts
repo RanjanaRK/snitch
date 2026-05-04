e20be3bc4ccb212b1282907a4fa281cf13d1faca
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-04 23:30:45 +0530

    storage service

---
+++ backend/src/service/storage.service.ts
@@ -0,0 +1,10 @@
+import ImageKit from "@imagekit/nodejs";
+import env from "../config/env.js";
+
+const client = new ImageKit({
+  privateKey: env.IMAGEKIT_PRIVATE_KEY,
+});
+
+export const uploadImage = async ({ folder, fileName, buffer }) => {
+  const response = await client.files.upload({});
+};
