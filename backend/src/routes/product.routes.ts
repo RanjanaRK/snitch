97f1808f2656aa7025094c6cd631e21d2f874542
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-05 22:48:13 +0530

    Update product.routes.ts

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { authenticateRole } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const productRouter = Router();
+
+productRouter.post(
+  "/",
+  authenticateRole(["seller"]),
+  upload.array("images", 7),
+  createProduct,
+);
 
 export default productRouter;
