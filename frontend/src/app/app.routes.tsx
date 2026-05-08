a49e1e3fc6fc3c1aae6aee9fab4ec38446531cb6
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-08 21:49:16 +0530

    Update app.routes.tsx

--- frontend/src/app/app.routes.tsx
+++ frontend/src/app/app.routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import CreateProduct from "../features/products/pages/CreateProduct";
+import Dashboard from "../features/products/pages/Dashboard";
 
 export const routes = createBrowserRouter([
   {
@@ -16,8 +17,18 @@ export const routes = createBrowserRouter([
     path: "/",
     element: <h1 className="text-center mt-20">Welcome to the Home Page</h1>,
   },
+
   {
-    path: "/seller/create-product",
-    element: <CreateProduct />,
+    path: "/seller",
+    children: [
+      {
+        path: "/seller/create-product",
+        element: <CreateProduct />,
+      },
+      {
+        path: "/seller/dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
