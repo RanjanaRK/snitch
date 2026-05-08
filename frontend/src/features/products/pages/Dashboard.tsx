6dcbbcf0aa726b1cc279d23fb962a8302c19b90a
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-08 21:46:11 +0530

    Create Dashboard.tsx

---
+++ frontend/src/features/products/pages/Dashboard.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useProduct } from "../hooks/useProduct";
+import { useSelector } from "react-redux";
+
+const Dashboard = () => {
+  const [products, setProducts] = useState([]);
+
+  const { handleGetSellerProducts } = useProduct();
+
+  const sellerProducts = useSelector(
+    (state: any) => state.product.sellerProduct,
+  );
+
+  useEffect(() => {
+    handleGetSellerProducts();
+  }, []);
+
+  console.log(sellerProducts);
+
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+};
+
+export default Dashboard;
