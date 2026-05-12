b7abdf65aa0cc741f0146864a5eef5c91d2a304d
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-12 15:58:03 +0530

    protected route

--- frontend/src/features/auth/components/Protected.tsx
+++ frontend/src/features/auth/components/Protected.tsx
@@ -1,11 +1,33 @@
 import type { ReactNode } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+import type { UserType } from "../utils/authTypes";
+import type { RootState } from "../../../app/app.store";
 
 type Props = {
   children: ReactNode;
+  role: string;
 };
 
-const Protected = ({ children }: Props) => {
-  return <>{children}</>;
+const Protected = ({ children, role = "buyer" }: Props) => {
+  const user = useSelector((state: RootState) => state.auth.user);
+  const loading = useSelector((state: RootState) => state.auth.loading);
+
+  console.log(user);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (user.role !== role) {
+    return <Navigate to="/" />;
+  }
+
+  return { children };
 };
 
 export default Protected;
