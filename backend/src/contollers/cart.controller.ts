e8a4b3a6064c2445a1ff3ad110e6e8400275463b
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-16 23:11:25 +0530

    Update cart.controller.ts

--- backend/src/contollers/cart.controller.ts
+++ backend/src/contollers/cart.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import productModel from "../model/product.model.js";
 import cartModel from "../model/cart.model.js";
 import type { JwtUser } from "../utils/types.js";
+import mongoose from "mongoose";
 
 export const addToCart = async (req: Request, res: Response) => {
   try {
@@ -28,9 +29,71 @@ export const addToCart = async (req: Request, res: Response) => {
       (variant) => variant._id.toString() === variantId,
     )?.stock;
 
+    if (!stock) {
+      return res
+        .status(404)
+        .json({ message: "Variant not found", success: false });
+    }
+
     const cart =
       (await cartModel.findOne({
         user: user.id,
       })) || (await cartModel.create({ user: user.id }));
-  } catch (error) {}
+
+    const isProductAlreadyIncart = cart.items.find(
+      (item) =>
+        item.product.toString() === productId &&
+        item.variant?.toString() === variantId,
+    );
+
+    if (isProductAlreadyIncart) {
+      const quantityInCart = cart.items.find(
+        (item) =>
+          item.product.toString() === productId &&
+          item.variant?.toString() === variantId,
+      )?.quantity;
+
+      if (quantityInCart + quantity > stock) {
+        return res
+          .status(400)
+          .json({ message: "Out of stock", success: false });
+      }
+
+      await cartModel.findOneAndUpdate(
+        {
+          user: user.id,
+          "items.product": productId,
+          "items.variant": variantId,
+        } as any,
+        { $inc: { "items.$.quantity": quantity } },
+        { new: true },
+      );
+
+      return res.status(200).json({
+        message: "Cart updated successfully",
+        success: true,
+      });
+    }
+
+    if (quantity > stock) {
+      return res
+        .status(400)
+        .json({ message: `Only ${stock} items left in stock`, success: false });
+    }
+
+    cart.items.push({
+      product: productId,
+      variant: variantId,
+      quantity,
+      price: product.price,
+    });
+
+    await cart.save();
+    return res.status(200).json({
+      message: "Product added to cart successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
+  }
 };
