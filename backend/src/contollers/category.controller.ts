bfa8982cc88291979483fd914c8a295971f6b218
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-28 21:39:36 +0530

    Update category.controller.ts

--- backend/src/contollers/category.controller.ts
+++ backend/src/contollers/category.controller.ts
@@ -1,7 +1,13 @@
 import type { Request, Response } from "express";
+import { categoryModel } from "../model/category.model.js";
 
 export const getCategoryController = async (req: Request, res: Response) => {
   try {
+
+
+    const categories= await categoryModel.find()
+
+
   } catch (error) {
     return res.status(500).json({ message: "Server error" });
   }
