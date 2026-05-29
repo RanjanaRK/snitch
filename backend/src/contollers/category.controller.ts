bfbc04175bae95729a6f65b337af40d09aaa410e
RanjanaRK <ranjanakumari.me@gmail.com>
2026-05-30 00:31:16 +0530

    Update category.controller.ts

--- backend/src/contollers/category.controller.ts
+++ backend/src/contollers/category.controller.ts
@@ -3,7 +3,9 @@ import { categoryModel } from "../model/category.model.js";
 
 export const getCategoryController = async (req: Request, res: Response) => {
   try {
-    const categories = await categoryModel.find();
+    const categories = await categoryModel.find({
+      parentCategory: null,
+    });
 
     return res.status(200).json({
       success: true,
@@ -18,4 +20,20 @@ export const getCategoryController = async (req: Request, res: Response) => {
 export const getSubCatgeoriesController = async (
   req: Request,
   res: Response,
-) => {};
+) => {
+  try {
+    const { parentId } = req.params as { parentId: string };
+
+    const subCategories = await categoryModel.find({
+      parentCategory: parentId,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Subcategory fetched",
+      subCategories,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
